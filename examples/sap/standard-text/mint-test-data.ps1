b0bc90d8f7147a13b9954c54131653cefce65b08
Happy Mahlangu <happysfisomahlangu12@gmail.com>
2026-08-12 17:10:25 +0200

    fix(sap): keep example artifact-only

--- examples/sap/standard-text/mint-test-data.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$suffix = [DateTime]::UtcNow.ToString('yyMMddHHmmss') + '-' + [Guid]::NewGuid().ToString('N').Substring(0, 6).ToUpperInvariant()
-Write-Output "SO10_TEXT_NAME=ZFP-$suffix"
